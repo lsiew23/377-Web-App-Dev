d2882faafe576dbca91c5cc92fe34002acfc21bb
leyton siew <lsiew23@hanoverstudents.org>
2022-09-27 12:47:32 -0400

    9/27 commit

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -1,14 +1,30 @@
-
+var numList = [];
 
 function calcVelocity() {
-        var distance = 0;
-        var time = 0;
-        var velocity = 0;
  
-        distance = document.getElementById("distance").value;
-        time = document.getElementById("time").value;
+        var distance = document.getElementById("distance").value;
+        var time = document.getElementById("time").value;
 
-        velocity = distance / time;
+        var velocity = distance / time + " m/s";
 
-        document.getElementById("velocity").innerHTML = velocity + " m/s";
+        document.getElementById("velocity").innerHTML = velocity;
 } 
+
+function avgNum() {
+        var sum = 0;
+        for (var i = 0; i < numList.length; i++) {
+                sum += numList[i];
+            }
+
+            var result = sum/numList.length;
+            document.getElementById("result").innerHTML = result;        
+}
+
+function addToList(){
+        var num = (parseInt(document.getElementById("num").value));
+        //num = document.getElementById("num").value;
+        document.getElementById("num").value = "";
+        numList.push(num);
+        document.getElementById("list").innerHTML += num + ", ";
+        
+}
