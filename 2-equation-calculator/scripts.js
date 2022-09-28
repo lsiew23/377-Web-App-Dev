65a81db64c53f901bcceceac3ece45bfdeeebedd
leyton siew <lsiew23@hanoverstudents.org>
2022-09-28 12:49:04 -0400

    9/28 commit

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -28,3 +28,26 @@ function addToList(){
         document.getElementById("list").innerHTML += num + ", ";
         
 }
+
+function calcError(){
+        var value1 = parseInt(document.getElementById("value1").value);
+        var value2 = parseInt(document.getElementById("value2").value);
+        var error1 = parseInt(document.getElementById("error1").value);
+        var error2 = parseInt(document.getElementById("error2").value);
+
+        var answer = value1 + value2;
+
+        document.getElementById("answer").innerHTML = answer;
+
+}
+
+function pythagorean(){
+        var a = parseInt(document.getElementById("a").value);
+        var b = parseInt(document.getElementById("b").value);
+        
+        var c = Math.sqrt((a * a) + (b * b));
+
+        document.getElementById("equation").innerHTML = a + "²" + " + " + b + "²" + " = " + c + "²";
+
+        document.getElementById("resultC").innerHTML = c;
+}
