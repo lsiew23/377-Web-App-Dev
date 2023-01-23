11e5cbfd6237ea9d77449b0be5c1f1f332bfb56c
Leyton Siew <lsiew23@hanoverstudents.org>
2023-01-23 12:18:09 -0500

    3.py fix

--- project-euler/3.py
+++ project-euler/3.py
@@ -1,15 +1,15 @@
 import math
 
-def largestPrimeFactor(num):
-    while num % 2 == 0:
-        largest_factor = 2
-        num = num / 2
-    for i in range(3, int(math.sqrt(num))+1, 2):
-        while num % i == 0:
-            largestFactor = i
-            num = num / i
-    if num > 2:
-        largestFactor = num
-    return int(largestFactor)
+num = 600851475143
 
-print(largestPrimeFactor(600851475143))
+while num%2 == 0:
+    lgf = 2
+    num = num/2
+for i in range(3, int(math.sqrt(num))+1, 2):
+    while num%i == 0:
+        lgf = i
+        num = num / i
+if num > 2:
+    lgf = num
+    
+print(lgf)
